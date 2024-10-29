dad5bf62445cdd7a17d9f1f6cfa6ca9f456ee62a
ArekStasko <arek.stasko@gmail.com>
2024-10-29 21:12:53 +0100

    Add ex

--- UniversityExercises/src/Main.java
+++ UniversityExercises/src/Main.java
@@ -2,11 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int a = 2;
-        long b = 3;
-        b = a;
-        // przypisanie B do A nie jest możliwe bez zastosowania castowania
-        // ze względu na mniejszą pojemność typu int
-        a = b;
+        int a = 4;
+        int b = 5;
+        int c = 3;
+        String message = a > b ?
+                (a > c  ? "a jest najwieksze" : "c jest najwieksze")
+                : (b > c  ? "b jest najwieksze" : "c jest najwieksze");
+        System.out.println(message);
     }
 }
