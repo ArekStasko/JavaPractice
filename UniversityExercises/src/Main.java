c8d50610ed6edc120433b03454697092bf7934fd
ArekStasko <arek.stasko@gmail.com>
2024-11-14 21:14:15 +0100

    Add ex

--- UniversityExercises/src/Main.java
+++ UniversityExercises/src/Main.java
@@ -1,7 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        Triangle.Run(1000);
+        char[] values = new char[10];
+        char[] mirror = new char[values.length];
+        System.out.println("Values:");
+        for(int i = 0; i < 10; i++) {
+            int randomInt = 97 + (int) (Math.random() * 26);
+            values[i] = (char)randomInt;
+            System.out.println(values[i]);
+        }
+        System.out.println("Mirror:");
+        for(int i = 0; i < mirror.length; i++) {
+            mirror[i] = values[values.length - 1 - i];
+            System.out.println(mirror[i]);
+        }
+
     }
 }
